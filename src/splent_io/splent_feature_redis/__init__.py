1f90088861ce65daaad6d461f847428d90c3a429
drorganvidez <davromorg@alum.us.es>
2026-03-27 00:35:34 +0000

    style: ruff lint+format, fix noqa annotations

--- src/splent_io/splent_feature_redis/__init__.py
+++ src/splent_io/splent_feature_redis/__init__.py
@@ -1,10 +1,11 @@
 from splent_framework.blueprints.base_blueprint import BaseBlueprint
 
-redis_bp = BaseBlueprint('redis', __name__, template_folder='templates')
+redis_bp = BaseBlueprint("redis", __name__, template_folder="templates")
 
 
 def init_feature(app):
     pass
+
 
 def inject_context_vars(app):
     return {}
--- src/splent_io/splent_feature_redis/config.py
+++ src/splent_io/splent_feature_redis/config.py
@@ -5,9 +5,11 @@ import redis
 def inject_config(app):
     redis_url = os.getenv("REDIS_URL", "redis://redis:6379")
 
-    app.config.update({
-        "SESSION_TYPE": "redis",
-        "SESSION_PERMANENT": False,
-        "REDIS_URL": redis_url,
-        "SESSION_REDIS": redis.from_url(redis_url)
-    })
+    app.config.update(
+        {
+            "SESSION_TYPE": "redis",
+            "SESSION_PERMANENT": False,
+            "REDIS_URL": redis_url,
+            "SESSION_REDIS": redis.from_url(redis_url),
+        }
+    )
