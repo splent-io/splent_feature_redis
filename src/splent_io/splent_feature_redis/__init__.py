e81204a35c242c70813beb45b9ac6cc442e92893
drorganvidez <davromorg@alum.us.es>
2026-04-02 01:35:02 +0000

    chore: release splent_io/splent_feature_redis to v1.5.5

--- src/splent_io/splent_feature_redis/__init__.py
+++ src/splent_io/splent_feature_redis/__init__.py
@@ -1,6 +1,6 @@
-from splent_framework.blueprints.base_blueprint import BaseBlueprint
+from splent_framework.blueprints.base_blueprint import create_blueprint
 
-redis_bp = BaseBlueprint("redis", __name__, template_folder="templates")
+redis_bp = create_blueprint(__name__)
 
 
 def init_feature(app):
